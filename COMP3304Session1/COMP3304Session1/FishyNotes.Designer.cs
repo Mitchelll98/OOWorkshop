74b4f10e4cfd21c51094f4d6f43b6e19e27190aa
Mitchelll98 <Mitchelll98@users.noreply.github.com>
2020-02-01 23:32:25 +0000

    Resize UI

--- COMP3304Session1/COMP3304Session1/FishyNotes.Designer.cs
+++ COMP3304Session1/COMP3304Session1/FishyNotes.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1561, 1132);
+            this.ClientSize = new System.Drawing.Size(513, 329);
             this.Controls.Add(this.button1);
             this.Name = "FishyNotes";
             this.Text = "FishyNotes";
